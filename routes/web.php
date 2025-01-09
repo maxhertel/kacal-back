66c929310cd3e4d2b87ea11539874b67e2b2d307
Max Hertel <maxherteljr@gmail.com>
2025-01-09 20:52:31 -0300

    criando sistema de login e permissões, e criação dee usrio

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['message' => 'Welcome to the API'];
+});
+
+Route::get('/status', function () {
+    return response()->json(['status' => 'API funcionando!']);
+});
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
